f68b4ba79ba974ec0afc2985ba6493aca29680fb
derthek <shape07_mrm@hotmail.com>
2019-10-01 15:50:19 +0200

    Updates

---
+++ precache-manifest.08ebc724d06f467a4dc28874267f04da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa7ac7e75f643838fbb952994c18692",
+    "url": "/DFDS.github.io/index.html"
+  },
+  {
+    "revision": "1403a9147c1176359a68",
+    "url": "/DFDS.github.io/static/css/main.6210ad8c.chunk.css"
+  },
+  {
+    "revision": "b63150b327024afccf22",
+    "url": "/DFDS.github.io/static/js/2.9bdd2024.chunk.js"
+  },
+  {
+    "revision": "1403a9147c1176359a68",
+    "url": "/DFDS.github.io/static/js/main.702e933d.chunk.js"
+  },
+  {
+    "revision": "8d308ce4e0ec39b61a80",
+    "url": "/DFDS.github.io/static/js/runtime~main.6ff67171.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a108288041ed9754221fb362690f1e7.js"
+  "/DFDS.github.io/precache-manifest.08ebc724d06f467a4dc28874267f04da.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DFDS.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
